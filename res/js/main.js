d40fb65a72d9ea59cca72dd0902edc3738abdd9b
gg <petrdovicin4@gmail.com>
2023-10-31 20:11:41 +0100

    cs

---
+++ res/js/main.js
@@ -0,0 +1,44 @@
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
+const autoClickerUpgrade = document.getElementById("autoClickerUpgrade");
+
+let numberOfCookies = 0;
+let cookieIncreaseNumber = 1;
+let autoclickerCookieIncreaseNumber = 0;
+let autoclickerInterval;
+let clickUpgradePrice = 50;
+
+cookie.onclick = () => {
+  numberOfCookies += cookieIncreaseNumber;
+
+  counter.innerText = "SNUS: " + numberOfCookies;
+};
+clickUpgrade.onclick = () => {
+  if (numberOfCookies >= clickUpgradePrice) {
+    //odecist cenu nakupu
+    numberOfCookies -= clickUpgradePrice;
+    clickUpgradePrice *= 2;
+    clickUpgrade.innerHTML = 'Buy click upgrade:'; 
+    counter.innerText = "SNUS: " + numberOfCookies;
+    //zvednout klikani o 1
+    cookieIncreaseNumber++;
+  }
+};
+
+autoClickerUpgrade.onclick = () => {
+  if (numberOfCookies >= 100) {
+    //odecteme cenu nakupu
+    numberOfCookies -= 100;
+    //zobrazeni textu
+    counter.innerText = "SNUS: " + numberOfCookies;
+    autoclickerCookieIncreaseNumber++;
+    //zastavime interval
+    clearInterval(autoclickerInterval);
+    //spustime autoclicker
+    autoclickerInterval = setInterval(() => {
+      numberOfCookies += autoclickerCookieIncreaseNumber;
+      counter.innerText = "SNUS: " + numberOfCookies;
+    }, 1000);
+  }
+}
