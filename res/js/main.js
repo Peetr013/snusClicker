44fe437b7c6d1d32ea56541a3bf17d69cb3cc0dc
gg <petrdovicin4@gmail.com>
2023-11-02 20:20:41 +0100

    snusIconUpgrade

--- res/js/main.js
+++ res/js/main.js
@@ -2,6 +2,7 @@ const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickUpgrade");
 const autoClickerUpgrade = document.getElementById("autoClickerUpgrade");
+const snusUpgrade = document.getElementById("snusUpgrade");
 
 let numberOfCookies = 0;
 let cookieIncreaseNumber = 1;
@@ -9,6 +10,7 @@ let autoclickerCookieIncreaseNumber = 0;
 let autoclickerInterval;
 let clickUpgradePrice = 50;
 let autoClickerUpgradePrice = 100;
+let snuscost = 10000;
 
 cookie.onclick = () => {
   numberOfCookies += cookieIncreaseNumber;
@@ -19,10 +21,9 @@ clickUpgrade.onclick = () => {
   if (numberOfCookies >= clickUpgradePrice) {
     //odecist cenu nakupu
     numberOfCookies -= clickUpgradePrice;
-    clickUpgradePrice += 100;
-    clickUpgrade.innerText = `Buy SNUS click upgrade: ${clickUpgradePrice} kč` ; 
-    //zvednout klikani o 1
-    cookieIncreaseNumber ++;
+    clickUpgradePrice += 325;
+    clickUpgrade.innerText = `More customers: ${clickUpgradePrice} kč`;
+    cookieIncreaseNumber += 3;
     counter.innerHTML = "SNUS: " + numberOfCookies;
   }
 };
@@ -31,10 +32,10 @@ autoClickerUpgrade.onclick = () => {
   if (numberOfCookies >= autoClickerUpgradePrice) {
     //odecteme cenu nakupu
     numberOfCookies -= autoClickerUpgradePrice;
-    autoClickerUpgradePrice += 150;
-    autoClickerUpgrade.innerText = `Buy Free Delivery of SNUS: ${autoClickerUpgradePrice} kč`
+    autoClickerUpgradePrice += 770;
+    autoClickerUpgrade.innerText = `Buy Delivery of SNUS: ${autoClickerUpgradePrice} kč`;
     counter.innerText = "SNUS: " + numberOfCookies;
-    autoclickerCookieIncreaseNumber += 2;
+    autoclickerCookieIncreaseNumber += 5;
     //zastavime interval
     clearInterval(autoclickerInterval);
     //spustime autoclicker
@@ -43,4 +44,35 @@ autoClickerUpgrade.onclick = () => {
       counter.innerHTML = "SNUS: " + numberOfCookies;
     }, 1000);
   }
+};
+snusUpgrade.onclick = () => {
+  if (numberOfCookies >= snuscost) {
+    cookieIncreaseNumber *= 2;
+    autoclickerCookieIncreaseNumber *= 2;
+    numberOfCookies -= snuscost;
+    counter.innerHTML = "SNUS: " + numberOfCookies;
+    if (cookie.getAttribute('src') == "./res/img/SNUSstage1.png") {
+      cookie.src = "./res/img/SNUSstage2.png";
+      snuscost = 100000;
+      snusUpgrade.innerHTML = `Buy better SNUS: ${snuscost} kč`  ;
+    } else if (cookie.getAttribute('src') == "./res/img/SNUSstage2.png") {
+      cookie.src = "./res/img/SNUSstage3.png";
+      snuscost = 500000;
+      snusUpgrade.innerHTML = `Buy better SNUS: ${snuscost} kč`  ;
+    } else if (cookie.getAttribute('src') == "./res/img/SNUSstage3.png") {
+      cookie.src = "./res/img/SNUSstage4.png";
+      snuscost = 1000000;
+      snusUpgrade.innerHTML = `Buy better SNUS: ${snuscost} kč` ;
+    } else if (cookie.getAttribute('src') == "./res/img/SNUSstage4.png") {
+      cookie.src = "./res/img/SNUSstage5.png";
+      snusUpgrade.innerText = "You have all SNUS ";
+    }else{
+
+    }
+
+  }
+
+
+
+  
 }
